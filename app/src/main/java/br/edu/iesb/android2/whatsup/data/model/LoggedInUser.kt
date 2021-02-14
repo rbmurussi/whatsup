15f461ac8494bfe3d4447c04b14c62108d44451a
rbmurussi <rbmurussi@gmail.com>
2021-02-14 17:12:09 -0300

    create projet iesb

---
+++ app/src/main/java/br/edu/iesb/android2/whatsup/data/model/LoggedInUser.kt
@@ -0,0 +1,9 @@
+package br.edu.iesb.android2.whatsup.data.model
+
+/**
+ * Data class that captures user information for logged in users retrieved from LoginRepository
+ */
+data class LoggedInUser(
+        val userId: String,
+        val displayName: String
+)
---
+++ app/src/main/java/br/edu/iesb/android2/whatsup/ui/login/LoginFormState.kt
@@ -0,0 +1,8 @@
+package br.edu.iesb.android2.whatsup.ui.login
+
+/**
+ * Data validation state of the login form.
+ */
+data class LoginFormState(val usernameError: Int? = null,
+                          val passwordError: Int? = null,
+                          val isDataValid: Boolean = false)
