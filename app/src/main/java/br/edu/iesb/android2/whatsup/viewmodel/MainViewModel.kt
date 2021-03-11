d5dc06ac553e2054a016f59651f3a514a6a18667
ALEXANDRE DIAS BRIGIDO <alexbrigido@hotmail.com>
2021-03-10 22:55:22 -0300

    ajustes no main view model

--- app/src/main/java/br/edu/iesb/android2/whatsup/viewmodel/MainViewModel.kt
+++ app/src/main/java/br/edu/iesb/android2/whatsup/viewmodel/MainViewModel.kt
@@ -4,12 +4,12 @@ import android.app.Application
 import androidx.lifecycle.AndroidViewModel
 import androidx.lifecycle.MutableLiveData
 import br.edu.iesb.android2.whatsup.domain.ItemResult
-import br.edu.iesb.android2.whatsup.util.Endpoint
+import br.edu.iesb.android2.whatsup.service.JsonPlaceholderService
 import br.edu.iesb.android2.whatsup.util.NetworkUtils
 import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.launch
-//import javax.inject.Inject
+import javax.inject.Inject
 import kotlin.coroutines.CoroutineContext
 
 class MainViewModel(val app: Application) : AndroidViewModel(app), CoroutineScope {
@@ -18,14 +18,13 @@ class MainViewModel(val app: Application) : AndroidViewModel(app), CoroutineScop
     val responseView = MutableLiveData<List<ItemResult>>();
 
 //    @Inject
-//    private lateinit var service : Endpoint
-
-    val retrofit = NetworkUtils.getRetrofitInstance("https://jsonplaceholder.typicode.com")
+//    private lateinit var service : JsonPlaceholderService
+    private val retrofit = NetworkUtils.getRetrofitInstance("https://jsonplaceholder.typicode.com")
 
     fun load() {
         launch {
-            val service = retrofit.create(Endpoint::class.java)
-            responseView.value = service.posts.execute().body()
+            val service = retrofit.create(JsonPlaceholderService::class.java)
+            responseView.value = service.getPosts()
         }
     }
 }
